640df0d3e71a1ae0b8a7b4eae8a615324674058d
H. Chase Stevens <chase@chasestevens.com>
2017-11-02 20:29:08 -0400

    Add code

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='tracing',
+    packages=['tracing'],
+    version='0.1.0',
+    description='Program execution tracing utilities.',
+    license='MIT',
+    author='H. Chase Stevens',
+    author_email='chase@chasestevens.com',
+    url='https://github.com/hchasestevens/tracing',
+    entry_points={},
+    keywords='tracing debug ipython',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ]
+)
---
+++ tracing/__init__.py
@@ -0,0 +1,60 @@
+import sys
+from contextlib import contextmanager
+from functools import partial
+
+try:
+    from IPython.core.magic import register_line_magic
+except ImportError:
+    register_line_magic = None
+
+
+@contextmanager
+def tracing(pattern=None, out=None):
+    """Print executed lines to stdout."""
+    _trace = partial(trace_line, pattern)
+
+    if out is None:
+        out = sys.stdout
+
+    with redirect_stdout(out):
+        sys.settrace(_trace)
+        try:
+            yield
+        finally:
+            sys.settrace(None)
+
+
+def trace_line(pattern, frame, event, arg):
+    try:
+        if pattern and not fnmatch(frame.f_code.co_filename, pattern):
+            return
+        print("{0.f_code.co_filename}: {0.f_lineno}".format(frame))
+    except Exception:
+        pass
+
+
+@contextmanager
+def redirect_stdout(out):
+    orig_stdout = sys.stdout
+    sys.stdout = out
+    try:
+        yield
+    finally:
+        sys.stdout = orig_stdout
+
+
+if callable(register_line_magic):
+    @register_line_magic
+    def trace(line):
+        """Usage: %trace (enable|disable) [file pattern] [output path]"""
+        args = line.split()
+        enable = args[0] in {'enable', 'on'}
+
+        if not enable:
+            sys.settrace(None)
+            sys.stdout = sys.__stdout__
+            return
+
+        pattern = args[1] if len(args) > 1 else None
+        sys.stdout = open(args[2], 'a') if len(args) > 2 else sys.stdout
+        sys.settrace(partial(trace_line, pattern))
